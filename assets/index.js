e9b32ae3077ab4e8f671cbade0bc80607f8c2eb3
Casey Levin <clevin8080@yahoo.com>
2019-10-17 20:13:09 -0600

    setting up click function and document.ready

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,17 @@
-function data() {
-    var search = $(this).attr("ref");
+$(document).ready(function() {
+    // on click for each id for pre set data.
+    $(document).on("click", Meme);
+    $(document).on("click", "#theOffice", memeData);
+    $(document).on("click", "#Nfl", memeData);
+    $(document).on("click", "#Funny", memeData);
+    $(document).on("click", "#Fail", memeData);
+});
+
+
+
+
+function memeData() {
+    var search = $(this).attr("role");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + title + "&api_key=L8YGVZbCd7HXg8g9n320OoCvUbKe9IIt&limit= 10 ";
 }
 $.ajax({
